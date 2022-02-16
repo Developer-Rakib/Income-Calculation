9af4de2b3af0c0295bf2614852e50e9c4972e1c0
Developer-Rakib <mdtazulislam6011@gmail.com>
2022-02-17 00:00:22 +0600

    design compelete

--- js/app.js
+++ js/app.js
@@ -1,66 +1,81 @@
-const chair = 25;
-const daw = 68;
-const loungo = 90;
+let incomeFeild = document.getElementById("income");
+let foodFeild = document.getElementById("food");
+let rentFeild = document.getElementById("rent");
+let clothesFeild = document.getElementById("clothes");
+let expenses = document.getElementById("total-expenses");
+let balance = document.getElementById("balance");
+let remaningBalance = document.getElementById("remaning-balance");
 
 
-function  clickFunction(sum, id, price) {
-    let feild = document.querySelector(id+"-input");
-    
-    if (sum == true) {
-        feild.value ++;
-    } else if(feild.value > 0){
-        feild.value --;
 
+ //   calculate fuction
+function calculate() {
+
+    if (isNaN(incomeFeild.value) == false && isNaN(foodFeild.value) == false && isNaN(rentFeild.value) == false && isNaN(clothesFeild.value) == false) {
+
+        let parseIncome = parseInt(foodFeild.value);
+        let parseFood = parseInt(foodFeild.value);
+        let parseRent = parseInt(rentFeild.value);
+        let parseClothe = parseInt(clothesFeild.value);
+        let totalExpenses =  parseFood + parseRent + parseClothe;
+
+        if (parseFood >= 0 && parseRent >= 0 && parseClothe >= 0 && parseIncome >= 0) {
+
+            if (totalExpenses < parseInt(incomeFeild.value)) {
+
+                expenses.innerText = totalExpenses;
+                let restBalace = parseInt(incomeFeild.value) - totalExpenses;
+                balance.innerText = restBalace;
+                remaningBalance.innerText = restBalace;
+
+            } else {
+                
+                alert("Your income is more than your Expenses ")
+            }
+            
+        } else {
+            alert("Wrong Input!! Please Enter Positive Value")
+        }
+
+    } else {
+        alert("Wrong Input!! Please Enter Number")
     }
-    document.querySelector(id+"-price").innerText = feild.value * price;
-    calcalut()
-
 }
 
-function calcalut(){
-    let chairTotal = document.querySelector("#chair-price").innerText;
-    let dawTotal = document.querySelector("#daw-price").innerText;
-    let loungoTotal = document.querySelector("#loungo-price").innerText;
-    let subTotal = document.querySelector("#sub-total")
-    subTotal.innerText = parseInt(chairTotal) + parseInt(dawTotal) + parseInt(loungoTotal);
-    let subTotalConvert = parseInt(subTotal.innerText);
-    let tax = ((5 / 100) * subTotalConvert).toFixed(2);
-    document.querySelector("#tax").innerText = tax;
-    document.querySelector("#total").innerText = subTotalConvert + parseInt(tax);
-     
+// savings Fuction
+function savingsFuction() {
+
+    let percentage = document.getElementById("save");
+    let percentageAccordingToIncome = (parseInt(percentage.value) / 100) * parseInt(incomeFeild.value);
+    let savaingAmount = (parseInt(percentage.value) / 100) * parseInt(balance.innerText);
+
+    if (isNaN(percentage.value) == false) {
+        if (percentageAccordingToIncome < parseInt(balance.innerText)) {
+            document.getElementById("savings-balace").innerText = savaingAmount;
+            remaningBalance.innerText = parseInt(balance.innerText) - savaingAmount;
+    
+        } else {
+            alert("Your Saving is more than your Available balance")
+        }
+    } else {
+        alert("Wrong Input!! Please Enter Number");
+    }
 }
 
-// chair 
-document.querySelector("#chair-plus").addEventListener("click", function  (e) {
-    let sum = true;
-    clickFunction(sum, "#chair", chair)
+// calculate
+document.getElementById("calc-submit-btn").addEventListener("click", function 
+ () {
+    calculate();
+    
+    // incomeFeild.value = "";
+    // foodFeild.value = "";
+    // rentFeild.value = "";
+    // clothesFeild.value = "";
 })
 
-document.querySelector("#chair-minus").addEventListener("click", function  (e) {
-    let sum = false;
-    clickFunction(sum, "#chair", chair)
-})
 
-
-
-// daw 
-document.querySelector("#daw-plus").addEventListener("click", function  (e) {
-    let sum = true;
-    clickFunction(sum, "#daw", daw)
-})
-
-document.querySelector("#daw-minus").addEventListener("click", function  (e) {
-    let sum = false;
-    clickFunction(sum, "#daw", daw)
-})
-
-// loungo
-document.querySelector("#loungo-plus").addEventListener("click", function  (e) {
-    let sum = true;
-    clickFunction(sum, "#loungo", loungo)
-})
-
-document.querySelector("#loungo-minus").addEventListener("click", function  (e) {
-    let sum = false;
-    clickFunction(sum, "#loungo", loungo)
+// savings
+document.getElementById("savings-btn").addEventListener("click", function 
+ () {
+    savingsFuction()
 })
